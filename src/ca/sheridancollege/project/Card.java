6f5784509f9c3ddd25d34d9a12fb902c4b46871e
Jaesung Lim <limjaes@sheridancollege.ca>
2023-06-27 17:04:59 -0400

    Update Card.java

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -2,24 +2,32 @@
  * SYST 17796 Project Base code.
  * Students can modify and extend to implement their game.
  * Add your name as an author and the date!
+ * 
+ * @author Group7: Jaesung Lim, Ly Trong Nhan, Mykyta Varnikov
+ * @date Jun 27, 2023
  */
 package ca.sheridancollege.project;
 
-/**
- * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
- * game. Students wishing to add to the code should remember to add themselves as a modifier.
- *
- * @author dancye
- */
-public abstract class Card {
-    //default modifier for child classes
+public class Card {
 
-    /**
-     * Students should implement this method for their specific children classes
-     *
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
-    @Override
-    public abstract String toString();
+  private final String rank;
+  private final String suit;
 
+  public Card(String rank, String suit) {
+    this.rank = rank;
+    this.suit = suit;
+  }
+
+  public String getRank() {
+    return rank;
+  }
+
+  public String getSuit() {
+    return suit;
+  }
+
+  @Override
+  public String toString() {
+    return rank + " of " + suit;
+  }
 }
