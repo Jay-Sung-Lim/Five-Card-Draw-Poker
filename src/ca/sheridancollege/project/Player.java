1550d261fb300bc7dfc8036dcc1e0003b89ed8d3
Jaesung Lim <limjaes@sheridancollege.ca>
2023-06-27 17:07:30 -0400

    Update Player.java

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -2,48 +2,53 @@
  * SYST 17796 Project Base code.
  * Students can modify and extend to implement their game.
  * Add your name as an author and the date!
+ * 
+ * @author Group7: Jaesung Lim, Ly Trong Nhan, Mykyta Varnikov
+ * @date Jun 27, 2023
  */
 package ca.sheridancollege.project;
 
-/**
- * A class that models each Player in the game. Players have an identifier, which should be unique.
- *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
- */
-public abstract class Player {
+import java.util.*;
 
-    private String name; //the unique name for this player
+public class Player {
 
-    /**
-     * A constructor that allows you to set the player's unique ID
-     *
-     * @param name the unique ID to assign to this player.
-     */
-    public Player(String name) {
-        this.name = name;
+  private final String name;
+  private List<Card> hand;
+
+  public Player(String name) {
+    this.name = name;
+    this.hand = new ArrayList<>();
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public List<Card> getHand() {
+    return hand;
+  }
+
+  public void setHand(List<Card> hand) {
+    this.hand = hand;
+  }
+
+  public void addCardToHand(Card card) {
+    hand.add(card);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Player ").append(name).append(":\n");
+    for (int i = 0; i < hand.size(); i++) {
+      sb
+        .append("[Card ")
+        .append(i + 1)
+        .append("]: ")
+        .append(hand.get(i))
+        .append("\n");
     }
-
-    /**
-     * @return the player name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * Ensure that the playerID is unique
-     *
-     * @param name the player name to set
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * The method to be overridden when you subclass the Player class with your specific type of Player and filled in
-     * with logic to play your game.
-     */
-    public abstract void play();
-
+    return sb.toString();
+  }
 }
+
